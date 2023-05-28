39e08a3ae3d5a9ca2aeeb624fc588450156aab48
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-05-29 02:20:45 +0530

    using layouts using mulitple ejs files

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ app.get('/work', function(req, res){
     
 })
 
+app.get('/about', function(req, res){
+    res.render('about');
+})
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("the server is running on port 3000");
