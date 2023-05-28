7782c2350250cf694219650d03481aa6a382a678
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-05-29 00:47:27 +0530

    add item to list

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 const express = require('express');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+let task = ["wake up early", "go to gym"];
 
 app.get('/', function(req, res){
     let date = new Date();
-    let val = date.getDay();
-    let day = "";
-    switch (val){
-        case 0: 
-            day = 'Sunday';
-            break;
-        case 1: 
-            day = 'Monday';
-            break;
-        case 2: 
-            day = 'Tuesday';
-            break;
-        case 3: 
-            day = 'Wednesday';
-            break;
-        case 4: 
-            day = 'Thursday';
-            break;
-        case 5: 
-            day = 'Friday';
-            break;
-        case 6: 
-            day = 'Saturday';
-            break;
-        default: 
-            day = 'error occurred';
+    const options = {
+        weekday: 'long', 
+        day: 'numeric', 
+        month: 'long', 
     }
+    let val = date.toLocaleDateString('en-US', options) ;
 
-    res.render('list', {dayToday: day});
+    res.render('list', {dayToday: val, itemAdded: task});
 })
 
-app.listen(3000, function(){
+app.post('/', function(req, res){
+    task.push(req.body.task);
+    res.redirect('/');
+})
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("the server is running on port 3000");
 })
